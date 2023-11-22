5451fd9987818c5c71ed83873b7a0f4ded270480
Omer Oreg <sarhatabaot1@gmail.com>
2023-11-22 11:30:08 +0200

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ dependencyResolutionManagement {
             library("configurate-hocon", "org.spongepowered","configurate-hocon").versionRef("configurate")
             library("configurate-gson", "org.spongepowered","configurate-gson").versionRef("configurate")
             library("configurate-yaml", "org.spongepowered","configurate-yaml").versionRef("configurate")
-            library("paper-api", "io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+            library("paper-api", "io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
         }
     }
 }
